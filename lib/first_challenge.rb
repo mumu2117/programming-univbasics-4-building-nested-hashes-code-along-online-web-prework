13fa9ed740d6823788600172a3a768c72e9c6454
algebraic-designer-6218 <jmiller@archersystems.us>
2019-11-20 19:58:39 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -7,7 +7,7 @@ def first_challenge
       :hero_friends => []
      }
      :capulet => {
-      :patriarch => {},
+      :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {},
       :heroine => {},
       :heroine_friends => []
