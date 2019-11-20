4688ad7658d9f010f1dc23ea15d60a3f2d2e10e3
algebraic-designer-6218 <jmiller@archersystems.us>
2019-11-20 19:56:12 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,6 +1,10 @@
 def first_challenge
    epic_tragedy = {
-     
+     :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
      
    }
 end
